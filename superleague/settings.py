11291c20004fd745f7cb29d07d2c9a38b49cb79d
gaspardhurez <hurezgaspard@gmail.com>
2024-01-27 01:19:40 +0100

    Migrated prod to PostgreSQL

--- superleague/settings.py
+++ superleague/settings.py
@@ -147,4 +147,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-print(DATABASES)
